273e838feca1b837a78de2c7518f0fd32c4d9d99
Marvin Kurland <bugemarvin@outlook.com>
2022-10-14 20:38:43 +0300

    creating a file in tmp dir

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -7,7 +7,7 @@
 
 file {"school": # creating tmp with school file
     path    => '/tmp/school',
-    ensure  => directory,
+    ensure  => present,
     content => 'I love Puppet',
     owner   => 'www-data',
     group   => 'www-data',
