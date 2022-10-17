8823b8480e4045d2a7655c9cf79fc53e719cd503
Marvin Kurland <bugemarvin@outlook.com>
2022-10-17 10:00:41 +0300

    creating a puppet file for config of school at the ect/ssh/config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,9 +3,11 @@ file_line {'/etc/ssh/ssh_config':
     ensure   => present,
     multiple => true,
     line     => 'IdentityFile ~/.ssh/school',\
+    name     => 'config_one',
 }
 file_line {'etc/ssh/ssh_config':
     ensure   => present,
     multiple => true,
     line     => 'PasswordAuthentication no',
+    name     => 'config_two',
 }
