bd02391c3f8ddb4d8b6e27ccc979081d635c4ff3
Marvin Kurland <bugemarvin@outlook.com>
2022-12-01 21:31:55 +0300

    function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,20 @@
 #!/usr.bin/python3
-'''function to return the number of Reddit API subscribers
-'''
+"""function to return the number of Reddit API subscribers
+"""
 import requests
 import json
 
 
 URL = 'https://www.reddit.com/'
-'''Calling reddit website
-'''
+"""Calling reddit website
+"""
 
 
 def number_of_subscribers(subreddit):
-    '''find number of subscribers from reddit.\
+    """find number of subscribers from reddit.\
                    if invalid subreddit return 0\
                          in order to avoid redirects add a User-Agent
-        '''
+    """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) G\
                 ecko/20100101 Firefox/91.0'}
