0c337a347b33817c37d2b26f150bbbe1fda8eee3
Marvin Kurland <bugemarvin@outlook.com>
2022-12-09 00:07:40 +0300

    setting open limit request in nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,25 @@
 # fixing nginx to handle extra request for open file
 
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['nginx'],
+}
+
+exec {'nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Service['nginx'],
+}
+
+service {'nginx':
+  ensure => running,
+  enable => true,
+  before => Exec['/etc/default/nginx'],
+}
+
 exec {'/etc/default/nginx':
-  command  => 'sudo echo "ulimit -n 4096" | sudo tee -a /etc/default/nginx; sudo service nginx restart',
+  command  => 'sudo echo "ULIMIT=\"-n 15\"\nulimit -n 4096" | sudo tee -a /etc/default/nginx; sudo service nginx restart',
   provider => shell,
 }
+
